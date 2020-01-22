1a20a43a68e94d3f2be74d713b1de8fd60550d2e
guticode04 <56804355+guticode04@users.noreply.github.com>
2020-01-21 23:20:02 -0800

    Add files via upload

---
+++ W9D1/first_rails_project/app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+    users = User.all 
+    render json: users
+  end
+
+  def create
+     user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: User.find(params[:id])
+  end
+
+  def update
+    #first find the user
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+    
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
+  end
+
+  protected
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
